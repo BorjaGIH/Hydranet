a2cb48c620b0eb70fd0841f0e890815b67df8cb8
bvelasco <velasco.6655@gmail.com>
2023-05-03 17:03:12 +0200

    Synthetic data generation

--- Input_data/data_generator.py
+++ Input_data/data_generator.py
@@ -10,6 +10,16 @@ dataset = 'synthetic' # or 'synthetic'
 output_parent_dir = '/home/bvelasco/Hydranet/Input_data'
 np.random.seed(1)
 
+
+def treatment_assignment_op(x, num_treats, p_yes=0.8, p_no=0.2):
+    t_vals = [i for i in range(num_treats)]
+    x_new=x.copy()
+    for j in t_vals:
+        probs = np.tile(p_no / (float(len(t_vals) - 1)), len(t_vals))
+        probs[j] = p_yes
+        x_new[x == j] = np.random.choice(t_vals, size=sum(x == j), replace=True, p=probs)
+    return x_new
+
 def plot_histograms(data, z, y_toplot, bins, path):
     ax = data[y_toplot].hist(bins=bins)
     data[y_toplot][data.z == 1].hist(bins=bins)
@@ -49,30 +59,14 @@ def analyse_generated_data(temp_all, path):
 
         ### Individual and total bias
 
-        sumfile.write('Bias 0: {}\n'.format(abs((temp_all['y_1'].mean() - temp_all['y_0'].mean()) - (
+        sumfile.write('Bias 1_0: {}\n'.format(abs((temp_all['y_1'].mean() - temp_all['y_0'].mean()) - (
                 temp_all.y[temp_all.z == 1].mean() - temp_all.y[temp_all.z == 0].mean()))))
-        sumfile.write('Bias 1: {}\n'.format(abs((temp_all['y_2'].mean() - temp_all['y_0'].mean()) - (
+        sumfile.write('Bias 2_0: {}\n'.format(abs((temp_all['y_2'].mean() - temp_all['y_0'].mean()) - (
                 temp_all.y[temp_all.z == 2].mean() - temp_all.y[temp_all.z == 0].mean()))))
-        sumfile.write('Bias 2: {}\n'.format(abs((temp_all['y_3'].mean() - temp_all['y_0'].mean()) - (
+        sumfile.write('Bias 3_0: {}\n'.format(abs((temp_all['y_3'].mean() - temp_all['y_0'].mean()) - (
                 temp_all.y[temp_all.z == 3].mean() - temp_all.y[temp_all.z == 0].mean()))))
-        sumfile.write('Bias 3: {}\n'.format(abs((temp_all['y_4'].mean() - temp_all['y_0'].mean()) - (
+        sumfile.write('Bias 4_0: {}\n'.format(abs((temp_all['y_4'].mean() - temp_all['y_0'].mean()) - (
                 temp_all.y[temp_all.z == 4].mean() - temp_all.y[temp_all.z == 0].mean()))))
-
-        b1 = abs((temp_all.y[temp_all.z == 1].mean() - temp_all.y[temp_all.z == 0].mean()) - (
-                temp_all.y_1 - temp_all.y_0).mean())
-        b2 = abs((temp_all.y[temp_all.z == 2].mean() - temp_all.y[temp_all.z == 0].mean()) - (
-                temp_all.y_2 - temp_all.y_0).mean())
-        b3 = abs((temp_all.y[temp_all.z == 3].mean() - temp_all.y[temp_all.z == 0].mean()) - (
-                temp_all.y_3 - temp_all.y_0).mean())
-        b4 = abs((temp_all.y[temp_all.z == 4].mean() - temp_all.y[temp_all.z == 0].mean()) - (
-                temp_all.y_4 - temp_all.y_0).mean())
-        sumfile.write('*******\n')
-
-        sumfile.write('Bias 0: {}\n'.format(b1))
-        sumfile.write('Bias 1: {}\n'.format(b2))
-        sumfile.write('Bias 2: {}\n'.format(b3))
-        sumfile.write('Bias 3: {}\n'.format(b4))
-        sumfile.write('*******\n')
 
         b1 = abs((temp_all.y[temp_all.z == 1].mean() - temp_all.y[temp_all.z == 0].mean()) - (
                 temp_all.y_1 - temp_all.y_0).mean()) / abs(temp_all.y_1 - temp_all.y_0).mean() * 100
@@ -94,7 +88,7 @@ def analyse_generated_data(temp_all, path):
     ### Individual histograms
 
     for z in range(num_treats):
-        plot_histograms(data=temp_all, z=z, y_toplot='y_{}'.format(z), bins=500, path=path)
+        plot_histograms(data=temp_all, z=z, y_toplot='y_{}'.format(z), bins=100, path=path)
 
 
 if dataset=='ihdp':
@@ -135,6 +129,8 @@ if dataset=='ihdp':
                                      'mu_2', 'mu_3', 'mu_4', 'y_0', 'y_1', 'y_2', 'y_3', 'y_4', 'y', 'z'])
 
     for i in range(num_reps):
+
+
         # Treatment assignment (probabilistic based on covariate, non-random)
         t_momblack = np.random.choice(t_vals, covars[covars.momblack == 1].shape[0], replace=True,p=np.array([0.8, 0.05, 0.05, 0.05, 0.05]))
         t_momwhite = np.random.choice(t_vals, covars[covars.momwhite == 1].shape[0], replace=True,p=np.array([0.05, 0.8, 0.05, 0.05, 0.05]))
@@ -200,52 +196,104 @@ if dataset=='ihdp':
     print('Done ihdp')
 
 elif dataset=='synthetic':
-    var_dict = {'bias':{'bias':[1,5,10,30], 'data_size':[2000], 'n_confs':2},
-                'n_confs':{'bias':10, 'data_size':[2000], 'n_confs':[2, 5, 10, 18]},
-                'data_size':{'bias':10, 'data_size':[1000, 2000, 5000, 10000], 'n_confs':2}
+    var_dict = {'bias':{'bias':[1, 10, 30, 50], 'data_size':2000, 'n_confs':2},
+                'n_confs':{'bias':30, 'data_size':2000, 'n_confs':[2, 5, 10, 18]},
+                'data_size':{'bias':30, 'data_size':[1000, 2000, 5000, 10000], 'n_confs':2}
                 }
 
-    for main_param in ['bias', 'data_size', 'n_confs']:
+    for main_param in ['bias','n_confs','data_size']:
 
-        bias = var_dict[main_param]['bias']
-        n_confs = var_dict[main_param]['n_confs']
-        data_size = var_dict[main_param]['data_size']
+        bias_vals = var_dict[main_param]['bias']
+        n_confs_vals = var_dict[main_param]['n_confs']
+        data_size_vals = var_dict[main_param]['data_size']
 
-        for i in var_dict[main_param][main_param]:
-            output_dir = os.path.join(output_parent_dir, dataset, '{}_treats/'.format(num_treats), main_param, str(i))
+        for i in range(len(var_dict[main_param][main_param])):
+
+            output_dir = os.path.join(output_parent_dir, dataset, '{}_treats/'.format(num_treats), main_param, str(var_dict[main_param][main_param][i]))
             print(output_dir)
 
-            for data_size_val in data_size:
+            if main_param=='bias':
+                bias=bias_vals[i]
+                data_size=data_size_vals
+                n_confs=n_confs_vals
+            elif main_param=='n_confs':
+                bias = bias_vals
+                data_size = data_size_vals
+                n_confs=n_confs_vals[i]
+            elif main_param=='data_size':
+                bias = bias_vals
+                data_size=data_size_vals[i]
+                n_confs = n_confs_vals
 
-                columns = ['x{}'.format(i) for i in range(num_covars)]+['mu_0', 'mu_1','mu_2', 'mu_3', 'mu_4', 'y_0', 'y_1', 'y_2', 'y_3', 'y_4', 'y', 'z']
-                temp_all = pd.DataFrame(columns=columns)
+            columns = ['x{}'.format(i) for i in range(num_covars)]+['mu_0', 'mu_1','mu_2', 'mu_3', 'mu_4', 'y_0', 'y_1', 'y_2', 'y_3', 'y_4', 'y', 'z']
+            temp_all = pd.DataFrame(columns=columns)
 
-                for j in range(num_reps):
-                    X = np.random.rand(data_size_val, num_covars)
-                    z = np.random.randint(low=0, high=num_treats, size=(data_size_val,1))
-                    y = np.random.randn(data_size_val,1)
-                    y0 = np.random.randn(data_size_val,1)
-                    y1 = np.random.randn(data_size_val,1)
-                    y2 = np.random.randn(data_size_val,1)
-                    y3 = np.random.randn(data_size_val,1)
-                    y4 = np.random.randn(data_size_val,1)
-                    mu_0 = np.random.randn(data_size_val,1)
-                    mu_1 = np.random.randn(data_size_val,1)
-                    mu_2 = np.random.randn(data_size_val,1)
-                    mu_3 = np.random.randn(data_size_val,1)
-                    mu_4 = np.random.randn(data_size_val,1)
+            for j in range(num_reps):
+                # Parameters
+                sigma=1
 
-                    covars_tab = np.concatenate([X,mu_0, mu_1, mu_2, mu_3, mu_4, y0, y1, y2, y3, y4, y, z], axis=1)
-                    covars_tab = pd.DataFrame(covars_tab, columns=columns)
+                # Covariates
+                X = np.random.rand(data_size, num_covars)
+                X = pd.DataFrame(X, columns=['x{}'.format(i) for i in range(num_covars)])
 
-                    filename = os.path.join(output_dir, 'ihdp_{}.csv'.format(j))
-                    os.makedirs(output_dir, exist_ok=True)
-                    covars_tab.to_csv(filename)
+                # Treatment
+                sel_covar_names = ['x{}'.format(i) for i in range(n_confs)]
+                covars = X[sel_covar_names]
+                z_ini = covars.sum(axis=1)
+                z = (num_treats*((z_ini-z_ini.min())/(z_ini.max()-z_ini.min()))).astype(int)
+                z = np.random.randint(low=0, high=num_treats, size=(data_size,1))
+                z_f = treatment_assignment_op(z, num_treats)
 
-                    # Accumulate dataset to compute statistics
-                    temp_all = pd.concat([temp_all, covars_tab], axis=0)
 
-                analyse_generated_data(temp_all, output_dir)
+                # Output
+                beta = 0.0001*np.random.randn(num_covars)
+                beta2 = 0.1*np.random.randn(n_confs+1)
+                #f = lambda x, tt: np.sqrt(np.abs(np.matmul(x, beta) + bias*np.matmul(np.c_[x[:,0:n_confs],tt**4], beta2))) # + 2*tt**2
+                f = lambda x,t: np.matmul(np.c_[x], beta) + np.log(np.abs(bias*np.matmul(np.c_[x[:,0:n_confs],t**2],beta2)**3)) + bias*np.matmul(np.c_[x[:,0:n_confs],t**2],beta2)
+                # Compute true effect
+                mu_0 = f(X.to_numpy(), 0 * np.ones(X.shape[0])).reshape(-1,1)
+                mu_1 = f(X.to_numpy(), 1 * np.ones(X.shape[0])).reshape(-1,1)
+                mu_2 = f(X.to_numpy(), 2 * np.ones(X.shape[0])).reshape(-1,1)
+                mu_3 = f(X.to_numpy(), 3 * np.ones(X.shape[0])).reshape(-1,1)
+                mu_4 = f(X.to_numpy(), 4 * np.ones(X.shape[0])).reshape(-1,1)
+
+                # Output
+                '''beta = np.random.randn(num_covars)
+                beta2 = np.random.randn(n_confs)
+                f = lambda x,t: np.matmul(np.c_[x,t], beta) + bias*np.matmul(np.c_[x[:,0:n_confs],t],beta2)
+                mu_0 = f(X.to_numpy()).reshape(-1, 1)
+                mu_1 = f(X.to_numpy()).reshape(-1, 1)
+                mu_2 = f(X.to_numpy()).reshape(-1, 1)
+                mu_3 = f(X.to_numpy()).reshape(-1, 1)
+                mu_4 = f(X.to_numpy()).reshape(-1, 1)'''
+
+
+                # Sample from normal distribution
+                y_f = lambda x: np.random.normal(x, sigma)
+                y_0 = y_f(mu_0).reshape(-1,1)
+                y_1 = y_f(mu_1).reshape(-1,1)
+                y_2 = y_f(mu_2).reshape(-1,1)
+                y_3 = y_f(mu_3).reshape(-1,1)
+                y_4 = y_f(mu_4).reshape(-1,1)
+
+                y = y_0.copy()
+                y[z == 0] = y_0[z == 0]
+                y[z == 1] = y_1[z == 1]
+                y[z == 2] = y_2[z == 2]
+                y[z == 3] = y_3[z == 3]
+                y[z == 4] = y_4[z == 4]
+
+                covars_tab = np.concatenate([X,mu_0, mu_1, mu_2, mu_3, mu_4, y_0, y_1, y_2, y_3, y_4, y, z], axis=1)
+                covars_tab = pd.DataFrame(covars_tab, columns=columns)
+
+                filename = os.path.join(output_dir, 'ihdp_{}.csv'.format(j))
+                os.makedirs(output_dir, exist_ok=True)
+                covars_tab.to_csv(filename)
+
+                # Accumulate dataset to compute statistics
+                temp_all = pd.concat([temp_all, covars_tab], axis=0)
+
+            analyse_generated_data(temp_all, output_dir)
 
 
     print('Done synthetic')
